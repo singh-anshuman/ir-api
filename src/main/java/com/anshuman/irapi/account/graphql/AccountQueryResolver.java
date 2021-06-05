13d31f174a2ea591b00d6da215a5ebb6a2a89a33
singh-anshuman <anshumansingh137@gmail.com>
2021-06-05 20:21:15 +0530

    Added querries for fetching accounts

---
+++ src/main/java/com/anshuman/irapi/account/graphql/AccountQueryResolver.java
@@ -0,0 +1,25 @@
+package com.anshuman.irapi.account.graphql;
+
+import com.anshuman.irapi.account.model.Account;
+import com.anshuman.irapi.account.service.AccountService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AccountQueryResolver implements GraphQLQueryResolver {
+
+    @Autowired
+    private AccountService accountService;
+
+    public List<Account> getAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    public Account getAccountById(Long id) {
+        return accountService.getAccountById(id).get();
+    }
+
+}
